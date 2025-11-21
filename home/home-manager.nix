6b8f4b2348064242cc84eb42306fe6cd28d6b79d
Che-0129 <yuto8664@gmail.com>
2025-11-21 17:56:16 +0900

    home-manager language

--- home/home-manager.nix
+++ home/home-manager.nix
@@ -14,6 +14,20 @@
         username = "che";
         homeDirectory = "/home/che";
         stateVersion = "25.11";
+        language = {
+            adress = "ja-JP.utf8";
+            base = "ja-JP.utf8";
+            collate = "ja-JP.utf8";
+            ctype = "ja-JP.utf8";
+            measurement = "ja-JP.utf8";
+            messages = "ja-JP.utf8";
+            monetary = "ja-JP.utf8";
+            name = "ja-JP.utf8";
+            numeric = "ja-JP.utf8";
+            paper = "ja-JP.utf8";
+            telephone = "ja-JP.utf8";
+            time = "ja-JP.utf8";
+        }
         file = {
             ".config/libfm".source = ./configs/libfm;
             ".config/mozc/config1.db".source = ./configs/mozc/config1.db;
