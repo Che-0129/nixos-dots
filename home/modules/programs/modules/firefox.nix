d564234d3c656f76928daf5ac7f9017315aa9370
Che-0129 <yuto8664@gmail.com>
2025-11-21 18:21:44 +0900

    edit firefox.nix etc...

--- home/modules/programs/modules/firefox.nix
+++ home/modules/programs/modules/firefox.nix
@@ -8,7 +8,10 @@
             bookmarks = {};
             settings = {
                 "sidebar.verticalTabs" = true;
+                "browser.tabs.closeWindowWithLastTab" = false;
+                "ui.key.menuAccessKeyFocuses" = false;
             };
         };
+        policies.RequestedLocales = [ "ja" ];
     };
 }
