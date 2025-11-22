009e11cc0f69991d2848c2d6e6bb8722f3e0db4c
Che-0129 <yuto8664@gmail.com>
2025-11-22 13:06:51 +0900

    edit swayidle.nix

--- home/modules/services/modules/swayidle.nix
+++ home/modules/services/modules/swayidle.nix
@@ -6,13 +6,13 @@
         extraArgs = [ "-w" ];
         events = [
             { event = "before-sleep"; command = "${pkgs.systemd}/bin/loginctl lock-session"; }
-            { event = "after-resume"; command = "/etc/profiles/per-user/$USER/bin/mmsg -d enable_monitor,eDP-1"; }
+            { event = "after-resume"; command = "/etc/profiles/per-user/$USER/bin/mmsg -d enable_monitor"; }
             { event = "lock"; command = "${pkgs.procps}/bin/pidof swaylock || ${pkgs.playerctl}/bin/playerctl pause ; ${pkgs.swaylock-effects}/bin/swaylock"; }
         ];
         timeouts = [
             { timeout = 530; command = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10"; resumeCommand = "${pkgs.brightnessctl}/bin/brightnessctl -r"; }
             { timeout = 600; command = "${pkgs.systemd}/bin/loginctl lock-session"; }
-            { timeout = 630; command = "/etc/profiles/per-user/$USER/bin/mmsg -d disable_monitor,eDP-1"; resumeCommand = "/etc/profiles/per-user/$USER/bin/mmsg -d enable_monitor,eDP-1"; }
+            { timeout = 630; command = "/etc/profiles/per-user/$USER/bin/mmsg -d disable_monitor"; resumeCommand = "/etc/profiles/per-user/$USER/bin/mmsg -d enable_monitor"; }
             { timeout = 720; command = "${pkgs.systemd}/bin/systemctl suspend"; }
         ];
     };
