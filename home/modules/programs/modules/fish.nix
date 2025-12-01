816d993ff7d1d0e1aacb974f0ecffbc0b4a13813
Che-0129 <yuto8664@gmail.com>
2025-12-01 12:20:51 +0900

    Change the alias for the Fish shell to abbr

--- home/modules/programs/modules/fish.nix
+++ home/modules/programs/modules/fish.nix
@@ -8,12 +8,10 @@
                 exec uwsm start mango-uwsm.desktop
             end
         '';
-        shellAliases = {
+        shellAbbrs = {
             dl = "cd ~/Downloads";
             rb = "systemctl reboot";
             sd = "systemctl poweroff";
-            nrb = "sudo nixos-rebuild switch --flake ~/.nixos-dots#NixOS --impure";
-            nfu = "nix flake update ~/.nixos-dots";
             rm = "trash-put";
             ls = "eza --icons";
             la = "eza --icons -a";
@@ -22,21 +20,6 @@
             cat = "bat --theme Nord -pp";
             c = "clear";
             cls = "clear && ls";
-            gc = "git clone";
-            ga = "git add";
-            gcm = "git commit -m";
-            gp = "git push";
-            aria2 = "aria2c -x16 -s16";
-            untar = "tar -xvf";
         };
-        shellInit = ''
-            function sudo
-                if test "$argv[1]" = "rm"
-                    command sudo trash-put $argv[2..-1]
-                else
-                    command sudo $argv
-                end
-            end
-        '';
     };
 }
